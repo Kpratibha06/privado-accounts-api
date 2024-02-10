9ef218a82a009d43b7e516f2afa0bb582079a80c
Vishesh <45128646+vishesh27k@users.noreply.github.com>
2024-02-11 00:51:45 +0530

    Update SignupD.java

--- src/main/java/ai/privado/demo/accounts/service/dto/SignupD.java
+++ src/main/java/ai/privado/demo/accounts/service/dto/SignupD.java
@@ -50,8 +50,8 @@ public class SignupD {
 		return password;
 	}
 
-	public void setPassword(String password) {
-		this.password = password;
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
 	public String getDob() {
