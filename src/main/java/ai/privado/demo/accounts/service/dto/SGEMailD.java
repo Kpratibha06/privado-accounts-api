cfeea1583e819a40a29929f37557f29068266bca
Vishesh <45128646+vishesh27k@users.noreply.github.com>
2024-03-23 16:49:20 +0530

    Update SGEMailD.java

--- src/main/java/ai/privado/demo/accounts/service/dto/SGEMailD.java
+++ src/main/java/ai/privado/demo/accounts/service/dto/SGEMailD.java
@@ -7,7 +7,18 @@ public class SGEMailD {
 	private String emailid;
 	private String subject;
 	private String msgBody;
-	
+	private String voice_recognition;
+
+	public String getEmailid() {
+		return emailid;
+	}
+	public String getVoice() {
+		return voice;
+	}
+
+	public void setVoice_recognition(String voice) {
+		this.voice_recognition = voice_recognition;
+	}
 
 	public void setEmailid(String emailid) {
 		this.emailid = emailid;
