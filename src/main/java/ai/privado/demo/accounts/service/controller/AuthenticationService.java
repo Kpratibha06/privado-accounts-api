aaf0c09b9d3981672a6422245a10961137604907
Vishesh <45128646+vishesh27k@users.noreply.github.com>
2024-03-25 19:40:39 +0530

    Update AuthenticationService.java

--- src/main/java/ai/privado/demo/accounts/service/controller/AuthenticationService.java
+++ src/main/java/ai/privado/demo/accounts/service/controller/AuthenticationService.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sendgrid.Method;
 import com.sendgrid.Request;
